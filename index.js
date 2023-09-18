98d03a7d23080fbe082cad11493103d1e5113bee
King-Hur Wu <king2510@gmail.com>
2023-09-18 11:59:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const card = [];
+function writeCards(name, adj) {
+    for (let i = 0; i < name.length; i++) {
+    
+    card[i] = `Thank you, ${name[i]}, for the wonderful ${adj} gift!`
+    debugger;
+    }
+    return card;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--)
+        
+    }
+}
